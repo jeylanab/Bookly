21ebdf6df47ece924d48196fb69b417882cc5e0a
jeylanab <jeylantesi@gmail.com>
2024-08-05 10:12:37 +0530

    added env file

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require('express');
 const expressLayouts = require("express-ejs-layouts")
 const app = express();
 
-// hook up the router folder to the module
-const indexRouter = require("./routes/index")
+// Hook up the router folder to the module
+const indexRouter = require("./routes/index");
 
-app.set("view engine", "ejs")
-app.set("views", __dirname + "/views")
-app.set("layout", "layouts/layout")
-app.use(expressLayouts)
-app.use(express.static("public"))
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
 
-// use the router folder in the server
-app.use("/", indexRouter)
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("Connected to mongoose"));
 
+// Use the router folder in the server
+app.use("/", indexRouter);
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
